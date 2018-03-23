103b9b8d9240f688818867c9c5b6fd3e398b7926
gaolizhan <1015994292@qq.com>
2018-03-24 00:28:36 +0800

    2018 3.22 Work glz

--- src/com/cmy/teach/work1/Work1glz.java
+++ src/com/cmy/teach/work1/Work1glz.java
@@ -3,27 +3,27 @@ package com.cmy.teach.work1;
 import javax.imageio.stream.FileImageInputStream;
 import java.io.File;
 import java.io.IOException;
-
 public class Work1glz {
     public static void main(String[] args) {
-        String path = "E:\\wenjianjia\\wendan.txt";
+        String path = "C:\\Users\\Administrator\\Desktop\\hu.jpg";
         File file = new File(path);
         if (!file.exists()) {
             System.out.println("图片不存在");
+            return;
         } else {
             System.out.println("图片存在");
         }
         try {
             FileImageInputStream picture = new FileImageInputStream(file);
             byte[] adc = new byte[1024];
-            picture.read(adc);
-            byte[] a = new byte[adc.length];
+            int count = picture.read(adc);
+            byte[] a = new byte[count];
             for (int i = 0; i < adc.length; i++) {
                 a[i] = adc[i];
             }
             String b = new String(a);
-            System.out.println(b.length());
             System.out.println(b);
+            System.out.println(b.length());
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/com/cmy/teach/work1/Work1quw.java
@@ -0,0 +1,4 @@
+package com.cmy.teach.work1;
+
+public class Work1quw {
+}
